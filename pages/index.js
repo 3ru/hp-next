99da480c4d35fb08d16d569ec4a956752974a830
Ryuyxx <ryuyxx@gmail.com>
2021-02-24 23:15:39 +0900

    feat: navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from 'next/head'
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1> Hello nextjs </h1>
+    <Layout title="Home">
+      <p className="text-4xl">Welcom to next HP</p>
 
       {/*<footer className={styles.footer}>*/}
       {/*  <a*/}
@@ -20,6 +15,6 @@ export default function Home() {
       {/*    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
       {/*  </a>*/}
       {/*</footer>*/}
-    </div>
-  )
+    </Layout>
+  );
 }
